7e778c23b3db939e21e29322745ebcaa2a61d79c
Nahom kiflu <kiflunahom1994@gmail.com>
2024-05-02 14:30:24 +0300

    task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""task takes k and v return tuple"""
+
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[float, int]) -> tuple[str, float]:
+    """
+    Returns a tuple where the first element is the string k
+    and the second element is the square of the int or float v.
+
+    Args:
+        k (str): The string key.
+        v (Union[int, float]): The integer or float value.
+
+    Returns:
+        Tuple[str, float]: A tuple containing the string k
+                           and the square of v as a float.
+    """
+    return (k, float(v ** 2))
