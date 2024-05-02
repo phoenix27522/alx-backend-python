bf614de1bbe2a9d916744cbc29c232a4d499ac69
Nahom kiflu <kiflunahom1994@gmail.com>
2024-05-02 13:27:56 +0300

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""task returning string """
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a floating-point number.
+
+    Args:
+        n (float): The input floating-point number.
+
+    Returns:
+        string: The string representation of the input number.
+    """
+    return str(n)
