be2c45f5d382842d7cd8155619a866b95945c240
Nahom kiflu <kiflunahom1994@gmail.com>
2024-05-02 13:35:19 +0300

    task 4

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''task 4 Annotating and defining variables with specified values .
+'''
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
