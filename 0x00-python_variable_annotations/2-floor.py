612299d10b6fb0be39bc4d86d4fc50094f567527
Nahom kiflu <kiflunahom1994@gmail.com>
2024-05-02 12:53:00 +0300

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a floating-point number.
+
+    Args:
+        n (float): The input floating-point number.
+
+    Returns:
+        int: The floor of the input number.
+    """
+    return int(n)
